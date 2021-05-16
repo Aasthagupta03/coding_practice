c2a16f0125d1c4ae492dfec7802c3088d33413f9
aastha <aasthagupta.ag71@gmail.com>
2021-05-16 23:05:59 +0530

    1.cpp

---
+++ 4.cpp
@@ -0,0 +1,54 @@
+// C++ program to sort an array
+// with 0, 1 and 2 in a single pass
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to sort the input array,
+// the array is assumed
+// to have values in {0, 1, 2}
+void sortArr(int a[], int arr_size)
+{
+	int lo = 0;
+	int hi = arr_size - 1;
+	int mid = 0;
+
+
+	while (mid <= hi) {
+		switch (a[mid]) {
+
+		// If the element is 0
+		case 0:
+			swap(a[lo++], a[mid++]);
+			break;
+
+		// If the element is 1 .
+		case 1:
+			mid++;
+			break;
+
+		// If the element is 2
+		case 2:
+			swap(a[mid], a[hi--]);
+			break;
+		}
+	}
+}
+
+
+
+int main()
+{
+	int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	sortArr(arr, n);
+
+
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+
+	return 0;
+}
+
+
